10ef6805caac3260809b2c12b4e325b5332fb3a5
chennamjaya <chennamjaya@gmail.com>
2023-11-27 21:08:37 +0530

    Created other parameters

--- main.tf
+++ main.tf
@@ -7,8 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "parameters" {
     default = [
-      {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"}, #here each value is a map
-      {name = "prod.rds.master_username", value = "expense", type = "String"}
+      { name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString" }, #here each value is a map
+      { name = "prod.rds.master_username", value = "expense", type = "String" },
+      { name = "prod.expense.frontend.backend_url", value = "http://backend-prod.vaishnavidevops.online/", type = "String" },
+      { name = "prod.rds.endpoint", value = "prod-mysql.cluster-cjgq1e8tovjw.us-east-1.rds.amazonaws.com", type = "String" }
     ]
 }
 
