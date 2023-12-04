9b500a96a69e80bf504c2bbff2123a9503ca0501
chennamjaya <chennamjaya@gmail.com>
2023-12-04 21:52:15 +0530

    Added jenkins_password parameter

--- main.tf
+++ main.tf
@@ -12,7 +12,10 @@ variable "parameters" {
       { name = "prod.expense.frontend.backend_url", value = "http://backend-prod.vaishnavidevops.online/", type = "String" },
       { name = "prod.rds.endpoint", value = "prod-mysql.cluster-cjgq1e8tovjw.us-east-1.rds.amazonaws.com", type = "String" },
 
-      { name = "dev.expense.frontend.backend_url", value = "http://backend-dev.rdevopsb73.online/", type = "String" }
+      { name = "dev.expense.frontend.backend_url", value = "http://backend-dev.rdevopsb73.online/", type = "String" },
+
+      { name = "jenkins_password", value = "admin1234", type = "SecureString" }
+
     ]
 }
 
